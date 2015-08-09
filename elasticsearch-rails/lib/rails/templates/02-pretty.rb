78ef3902f2fa229b10ed3ddfd4f665f5912b4245
noname00000123 <noname00000123@users.noreply.github.com>
2015-08-09 21:01:18 +1000

    Edited ln 58
    Added line to create file 'app/models/article.rb'

--- elasticsearch-rails/lib/rails/templates/02-pretty.rb
+++ elasticsearch-rails/lib/rails/templates/02-pretty.rb
@@ -7,16 +7,12 @@ say_status  "README", "Updating Readme...\n", :yellow
 puts        '-'*80, ''; sleep 0.25
 
 append_to_file 'README.rdoc', <<-README
-
 == [2] Pretty
-
 The `pretty` template builds on the `basic` version and brings couple of improvements:
-
 * Using the [Bootstrap](http://getbootstrap.com) framework to enhance the visual style of the application
 * Using an `Article.search` class method to customize the default search definition
 * Highlighting matching phrases in search results
 * Paginating results with Kaminari
-
 README
 
 git add:    "README.rdoc"
@@ -44,10 +40,8 @@ puts        '-'*80, ''; sleep 0.25
 # NOTE: Kaminari has to be loaded before Elasticsearch::Model so the callbacks are executed
 #
 insert_into_file 'Gemfile', <<-CODE, before: /gem ["']elasticsearch["'].+$/
-
 # NOTE: Kaminari has to be loaded before Elasticsearch::Model so the callbacks are executed
 gem 'kaminari'
-
 CODE
 
 run "bundle install"
@@ -61,9 +55,8 @@ puts
 say_status  "Model", "Adding a `Article.search` class method...\n", :yellow
 puts        '-'*80, ''; sleep 0.5
 
-insert_into_file 'app/models/article.rb', <<-CODE, after: 'include Elasticsearch::Model::Callbacks'
-
-
+run "rm -f app/models/article.rb"
+file 'app/models/article.rb', <<-CODE, after: 'include Elasticsearch::Model::Callbacks'
   def self.search(query)
     __elasticsearch__.search(
       {
@@ -87,21 +80,17 @@ insert_into_file 'app/models/article.rb', <<-CODE, after: 'include Elasticsearch
 CODE
 
 insert_into_file "#{Rails::VERSION::STRING > '4' ? 'test/models' : 'test/unit' }/article_test.rb", <<-CODE, after: /class ArticleTest < ActiveSupport::TestCase$/
-
   teardown do
     Article.__elasticsearch__.unstub(:search)
   end
-
 CODE
 
 gsub_file "#{Rails::VERSION::STRING > '4' ? 'test/models' : 'test/unit' }/article_test.rb", %r{# test "the truth" do.*?# end}m, <<-CODE
-
   test "has a search method delegating to __elasticsearch__" do
     Article.__elasticsearch__.expects(:search).with do |definition|
       assert_equal 'foo', definition[:query][:multi_match][:query]
       true
     end
-
     Article.search 'foo'
   end
 CODE
@@ -139,7 +128,6 @@ gsub_file 'app/views/articles/index.html.erb', %r{<%= label_tag .* :search %>}m 
 <<-CODE
 <div class="input-group">
   <%= text_field_tag :q, params[:q], class: 'form-control', placeholder: 'Search...' %>
-
   <span class="input-group-btn">
     <button type="button" class="btn btn-default">
       <span class="glyphicon glyphicon-search"></span>
@@ -170,7 +158,6 @@ gsub_file 'app/views/articles/index.html.erb', %r{<br>\s*(<\%= link_to 'New Arti
   replace = content.match(%r{<br>\s*(<\%= link_to 'New Article'.*)}m)[1]
   <<-END.gsub(/^  /, '')
   <hr>
-
   <p style="text-align: center; margin-bottom: 21px">
     #{replace}
   </p>
@@ -218,8 +205,6 @@ end
 
 insert_into_file 'app/views/articles/index.html.erb', after: '</table>' do
   <<-CODE.gsub(/^  /, '')
-
-
   <div class="text-center">
     <%= paginate @articles %>
   </div>
@@ -242,7 +227,6 @@ puts        '-'*80, ''; sleep 0.5
 append_to_file 'app/assets/stylesheets/application.css' do
   unless File.read('app/assets/stylesheets/application.css').include?('.label-highlight')
 <<-CODE
-
 .label-highlight {
   font-size: 100% !important;
   font-weight: inherit !important;
@@ -250,16 +234,13 @@ append_to_file 'app/assets/stylesheets/application.css' do
   color: #333 !important;
   background: #fff401 !important;
 }
-
 div.pagination {
   text-align: center;
   display: block;
 }
-
 div.pagination ul {
   display: inline-block;
 }
-
 CODE
   else
     ''
